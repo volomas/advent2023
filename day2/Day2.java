a3728c9474b2714ad9a9ac4eef1c06a4b7ab60d2
Volodymyr Masliy <thebestvova@gmail.com>
2023-12-02 12:42:04 +0100

    day2

--- day2/Day2.java
+++ day2/Day2.java
@@ -1,10 +1,34 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Day2 {
   public static void main(String[] args) throws IOException {
     Path path = Path.of(args[0]);
-    Files.lines(path).forEach(System.out::println);
+    int res = Files.lines(path)
+        .map(Day2::colorProduct)
+        .mapToInt(i -> i).sum();
+
+    System.out.println(STR. "part 2: \{ res }" );
+  }
+
+  private static int colorProduct(String line) {
+    String[] sets = line.split(":")[1].split(";");
+    var colors = new HashMap<>(Map.of("red", 1, "green", 1, "blue", 1));
+    Arrays.stream(sets)
+        .map(s -> s.split(", "))
+        .flatMap(Arrays::stream)
+        .map(String::trim)
+        .map(cube -> cube.split(" "))
+        .forEach(cube -> {
+          int num = Integer.parseInt(cube[0]);
+          String color = cube[1];
+          colors.compute(color, (k, v) -> num > v ? num : v);
+        });
+
+    return colors.values().stream().reduce(1, (i, j) -> i * j);
   }
 }
