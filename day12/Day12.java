4971c2516485a796b1028a6c376efef94266334f
Volodymyr Masliy <thebestvova@gmail.com>
2023-12-13 00:25:52 +0100

    day12 part 2

--- day12/Day12.java
+++ day12/Day12.java
@@ -1,10 +1,15 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Day12 {
+  static Map<String, Long> memo = new HashMap<>();
+
   public static void main(String[] args) throws IOException {
 //    solve("day12/sample.txt");
     solve("day12/input.txt");
@@ -12,18 +17,37 @@ public class Day12 {
 
   private static void solve(String filename) throws IOException {
     List<String> lines = Files.readAllLines(Path.of(filename));
-    int sum = 0;
+    long sum = 0;
     for (String line : lines) {
       String[] parts = line.split("\s+");
-      String record = parts[0];
+      String pattern = parts[0];
       List<Integer> numbers = Arrays.stream(parts[1].split(",")).map(Integer::parseInt).toList();
-      sum += countWays(record, numbers);
+
+      pattern = unfoldPattern(pattern);
+      numbers = unfoldNums(numbers);
+      sum += countWays(pattern, numbers);
     }
 
     System.out.println(STR. "Total arrengements: \{ sum }" );
   }
 
-  private static int countWays(String pattern, List<Integer> nums) {
+  private static List<Integer> unfoldNums(List<Integer> numbers) {
+    List<Integer> res = new ArrayList<>();
+    for (int i = 0; i < 5; i++) {
+      res.addAll(numbers);
+    }
+    return res;
+  }
+
+  private static String unfoldPattern(String pattern) {
+    List<String> patterns = new ArrayList<>();
+    for (int i = 0; i < 5; i++) {
+      patterns.add(pattern);
+    }
+    return String.join("?", patterns);
+  }
+
+  private static long countWays(String pattern, List<Integer> nums) {
     if (pattern.isEmpty()) {
       return nums.isEmpty() ? 1 : 0;
     }
@@ -32,7 +56,12 @@ public class Day12 {
       return pattern.contains("#") ? 0 : 1;
     }
 
-    int result = 0;
+    String key = pattern + "_" + nums;
+    if (memo.containsKey(key)) {
+      return memo.get(key);
+    }
+
+    long result = 0;
     char spring = pattern.charAt(0);
     if (spring == '.' || spring == '?') {
       result += countWays(pattern.substring(1), nums);
@@ -47,6 +76,8 @@ public class Day12 {
             nums.stream().skip(1).toList());
       }
     }
+
+    memo.put(key, result);
     return result;
   }
 }
