d493c1888cb0000c84c91bd465099547418ef6b4
Volodymyr Masliy <thebestvova@gmail.com>
2023-12-15 13:48:31 +0100

    day 15 part 2 using java hashmap
    Java's HashMap works exactly as described.
    The only trick is to properly calculate "slots"

--- day15/Day15.java
+++ day15/Day15.java
@@ -1,95 +1,61 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Day15 {
   public static final int BUCKETS = 256;
-  //todo I wonder if Java's HashMap would work?
-  static LinkedList<Label>[] hashMap = new LinkedList[BUCKETS];
+  static Map<Label, Integer> hashMap = new HashMap<>(BUCKETS);
 
   public static void main(String[] args) throws IOException {
 //    solve("day15/sample.txt");
     solve("day15/input.txt");
   }
 
-  private record Label(String label, int focalLen) {
-    @Override
-    public boolean equals(Object o) {
-      if (o instanceof Label) {
-        return Objects.equals(label, ((Label) o).label());
-      }
-      throw new IllegalStateException();
-    }
-
+  private record Label(String label) {
     @Override
     public int hashCode() {
-      return hash(label);
+      int h = 0;
+      for (char c : label.toCharArray()) {
+        h += c;
+        h *= 17;
+        h %= BUCKETS;
+      }
+      return h;
     }
 
     public int bucket() {
-      return hash(label) % BUCKETS;
-    }
-
-    @Override
-    public String toString() {
-      return STR. "[\{ label } \{ focalLen }]" ;
+      return hashCode() % BUCKETS;
     }
   }
 
   private static void solve(String filename) throws IOException {
     String input = Files.readString(Path.of(filename)).replaceAll("\n", "");
-    int sum = 0;
     for (String seq : input.split(",")) {
       if (seq.contains("-")) {
         String label = seq.replace("-", "");
-        Label l = new Label(label, 0);
-        LinkedList<Label> labels = hashMap[l.bucket()];
-        if (labels != null) {
-          labels.remove(l);
-        }
+        hashMap.remove(new Label(label));
       } else {
         String label = seq.split("=")[0];
         int focalLen = Integer.parseInt(seq.split("=")[1]);
-        Label l = new Label(label, focalLen);
-        LinkedList<Label> lenses = hashMap[l.bucket()];
-        if (lenses == null) {
-          hashMap[l.bucket()] = new LinkedList<>(List.of(l));
-        } else {
-          int slot = lenses.indexOf(l);
-          if (slot >= 0) {
-            lenses.remove(slot);
-            lenses.add(slot, l);
-          } else {
-            lenses.addLast(l);
-          }
-        }
+        hashMap.put(new Label(label), focalLen);
       }
     }
 
-    for (int i = 0; i < hashMap.length; i++) {
-      if (hashMap[i] != null) {
-        System.out.println(STR. "Bucket \{ i }: \{ hashMap[i] }" );
-        for (int slot = 1; slot <= hashMap[i].size(); slot++) {
-          Label label = hashMap[i].get(slot - 1);
-          int power = (i + 1) * slot * label.focalLen;
-          sum += power;
-        }
+    int sum = 0;
+    var slot = 1;
+    var prevBucket = -1;
+    for (var entry : hashMap.entrySet()) {
+      int bucket = entry.getKey().bucket();
+      if (prevBucket == bucket) {
+        slot++;
+      } else {
+        prevBucket = bucket;
+        slot = 1;
       }
+      sum += (bucket + 1) * entry.getValue() * slot;
     }
-
     System.out.println(STR. "Result = \{ sum }" );
-  }
-
-  private static int hash(String seq) {
-    int h = 0;
-    for (char c : seq.toCharArray()) {
-      h += c;
-      h *= 17;
-      h %= BUCKETS;
-    }
-    return h;
   }
 }
